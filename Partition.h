a833a3e4ab30b2f9e871663106bfdf6f818720fc
haojiangfeng <vujn2003@163.com>
2015-08-31 14:08:01 +0800

    f

--- Partition.h
+++ Partition.h
@@ -14,9 +14,13 @@ public:
 public:
 
 	bool IsPartitionFace();
+
 	vector<SFace*> OcctSplit();		//occt切割面
+
 	void JudgeCommon(SFace*  Fa, SFace* Fb);// 两个面相交或者多个面相交
+
 	void TraverseEdgeCurve(vector<stp_edge_curve*>& edgeList);
+
 	vector<SFace*> intersectionFaceList_;
 };
 
